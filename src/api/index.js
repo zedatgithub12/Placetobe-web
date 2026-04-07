d535e10c3166063621f330233255d55c5e814ece
zerihuntegenu <zerihun.tegenu@eaglelionsystems.com>
2026-04-07 13:03:54 +0300

    change the api base url

--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,11 @@
 const Connections = {
     // api: 'https://app.p2b-ethiopia.com/placetobe-dev/',
     // api: 'https://app.p2b-ethiopia.com/placetobe/',
-    // api: 'https://backend.placetobeethiopia.com/api/',
+    api: 'https://backend.placetobeethiopias.com/api/',
     // api: 'http://localhost:8000/api/',
 
     //testing endpoints
-    api: 'http://test.placetobeethiopia.com/service/api/',
+    // api: 'http://test.placetobeethiopia.com/service/api/',
 
     assets: 'images/',
     signUp: 'signup',
